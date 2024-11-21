98c059eaead30ae03384455b54a347238c624d7e
d1solis <d1solis@ucsd.edu>
2024-11-21 14:45:28 -0800

    Made some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
