e84cdd428de94c61c157f7210cb49d774ff4fc22
d1solis <d1solis@ucsd.edu>
2024-11-21 23:13:43 -0800

    Working proj

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
